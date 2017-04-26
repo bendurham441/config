199e1f3df4e0fe45c7ba111ed1d9ef4a861e18d7
Ben Durham <bendurham441@gmail.com>
2017-04-26 06:20:34 -0400

    Fixing files

--- get_data.sh
+++ get_data.sh
@@ -1,1 +1,6 @@
-../../.config/pianobar/get_data.sh
+#!/bin/sh
+
+artist=$(sed -n 's/^artist=\(.*\)/\1/p' /home/bendurham441/.config/pianobar/nowplaying)
+title=$(sed -n 's/^title=\(.*\)/\1/p' /home/bendurham441/.config/pianobar/nowplaying)
+
+echo "M$artist - $title"
